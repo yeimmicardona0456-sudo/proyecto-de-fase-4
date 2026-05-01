5645de4af5c41eeaddc187bfebbacb3164cc0e38
yeimmicardona0456-sudo <yeimmicardona.0456@gmail.com>
2026-05-01 16:42:13 -0500

    Add files via upload

---
+++ excepcione.py
@@ -0,0 +1,11 @@
+class ErrorSistema(Exception):
+    pass
+
+class ErrorCliente(ErrorSistema):
+    pass
+
+class ErrorReserva(ErrorSistema):
+    pass
+
+class ErrorServicio(ErrorSistema):
+    pass
